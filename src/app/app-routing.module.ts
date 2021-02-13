9c4625139dd3a921cfe9ba8f92e93e4ed9b35e82
sonhle9 <sonhle9@gmail.com>
2021-02-13 09:41:15 +0700

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionsNewComponent } from './sessions-new/sessions-new.component';
+import { StaticPagesAboutComponent } from './static-pages-about/static-pages-about.component';
+import { StaticPagesContactComponent } from './static-pages-contact/static-pages-contact.component';
+import { StaticPagesHelpComponent } from './static-pages-help/static-pages-help.component';
+import { StaticPagesHomeComponent } from './static-pages-home/static-pages-home.component';
+import { UsersEditComponent } from './users-edit/users-edit.component';
+import { UsersIndexComponent } from './users-index/users-index.component';
+import { UsersNewComponent } from './users-new/users-new.component';
+import { UsersShowComponent } from './users-show/users-show.component';
+import { UsersShowFollowersComponent } from './users-show-followers/users-show-followers.component';
+import { UsersShowFollowingComponent } from './users-show-following/users-show-following.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: StaticPagesHomeComponent },
+  { path: "about", component: StaticPagesAboutComponent },
+  { path: "contact", component: StaticPagesContactComponent },
+  { path: "help", component: StaticPagesHelpComponent },
+  { path: "users", component: UsersIndexComponent },
+  { path: "users/new", component: UsersNewComponent },
+  { path: "users/:id", component: UsersShowComponent },
+  { path: "users/:id/edit", component: UsersEditComponent },
+  { path: "signup", component: UsersNewComponent },
+  { path: "login", component: SessionsNewComponent },
+  { path: "users/:id/following", component: UsersShowFollowingComponent },
+  { path: "users/:id/followers", component: UsersShowFollowersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
