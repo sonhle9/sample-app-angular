d701cbb8fff6e24eaeb43fb6977e59ddef889122
sonhle9 <sonhle9@gmail.com>
2021-02-17 18:59:38 +0700

    add express server

---
+++ server/README.md
@@ -0,0 +1,6 @@
+Angular Forms Tutorial - 13 - Express Server to Receive Form Data
+https://www.youtube.com/watch?v=jwA-9XXybdM&list=PLC3y8-rFHvwhBRAgFinJR8KHIrCdTkZcZ&index=45
+
+npm init --yes
+npm i --save express body-parser cors
+node server
--- src/app/users-new/users-new.component.ts
+++ src/app/users-new/users-new.component.ts
@@ -9,33 +9,36 @@ import { EnrollmentService } from '../enrollment.service';
 })
 export class UsersNewComponent implements OnInit {
 
-  topics = ['Angular', 'React', 'Vue'];
-  userModel = new User('Rob', 'rob@test.com', 5556665566, 'default', 'morning', true);
-  topicHasError = true;
-  submitted = false;
-  errorMsg = '';
+  // topics = ['Angular', 'React', 'Vue'];
+  // userModel = new User('Rob', 'rob@test.com', 5556665566, 'default', 'morning', true);
+  // topicHasError = true;
+  // submitted = false;
+  // errorMsg = '';
 
-  constructor(private _enrollmentService: EnrollmentService) {}
+  // constructor(private _enrollmentService: EnrollmentService) {}
+  constructor() {}
 
   ngOnInit(): void {
   }
 
-  validateTopic(value:any) {
-    if (value === 'default') {
-      this.topicHasError = true;
-    } else {
-      this.topicHasError = false;
-    }
-  }
+  // validateTopic(value:any) {
+  //   if (value === 'default') {
+  //     this.topicHasError = true;
+  //   } else {
+  //     this.topicHasError = false;
+  //   }
+  // }
 
-  onSubmit() {
-    console.log(this.userModel)
-    this.submitted = true;
-    this._enrollmentService.enroll(this.userModel)
-      .subscribe(
-        response => console.log('Success!', response),
-        error => this.errorMsg = error.statusText
-      )
-  }
+  // // onSubmit(userForm:any) {
+  // onSubmit() {
+  //   // console.log(userForm) (ngSubmit)="onSubmit(userForm)"
+  //   console.log(this.userModel)
+  //   this.submitted = true;
+  //   this._enrollmentService.enroll(this.userModel)
+  //     .subscribe(
+  //       response => console.log('Success!', response),
+  //       error => this.errorMsg = error.statusText
+  //     )
+  // }
 
 }
